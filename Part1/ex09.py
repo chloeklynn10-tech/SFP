f5c7a0359e6cb4bfde45a3bab04b83e0135baa0a
chloeklynn10-tech <chloeklynn10@gmail.com>
2025-09-29 06:36:32 +0000

    finally done for part 1 ya

--- Part1/ex09.py
+++ Part1/ex09.py
@@ -1,15 +1,6 @@
-# Original text
-string1 = "heLlO mY Cat"
+# Current speed
+speed = 61
+print(speed)
 
-#convert 
-string_lower = string1.lower()
-string_upper = string1.upper()
-string_swapcase = string1.swapcase()
-string_capitalize = string1.capitalize()
-
-#print
-print(string1)
-print(string_lower)
-print(string_upper)
-print(string_swapcase)
-print(string_capitalize)
+if speed > 60:
+    print("over the speed limit!")
